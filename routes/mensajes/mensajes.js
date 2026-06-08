d22e992c58e145c1f73cfc00ef61a16a8244ff64
LaloSL <127783873+LaloSL@users.noreply.github.com>
2026-06-08 16:59:35 -0300

    Rutas y controller de Mensajes

---
+++ routes/mensajes/mensajes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const mensajesController = require('../controllers/mensajes/mensajesController');
+const { requireAuth } = require('../middleware/auth');
+
+router.get('/', requireAuth, mensajesController.recibidos);
+
+router.get('/:usuarioId', requireAuth, mensajesController.conversacion);
+
+router.post('/:usuarioId', requireAuth, mensajesController.enviar);
+
+module.exports = router;
